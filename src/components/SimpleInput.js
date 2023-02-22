85459a5a5d6c0758dc9faa188e6f6d10399f118a
hstefanova <hristiyana.stefanova@mentormate.com>
2023-02-22 11:24:19 +0200

    a way to validate input field

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,61 @@
+import { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+  const [enteredNameIsTouched, setEnteredNameIsTouched] = useState(false);
+
+  const nameChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+
+    if (enteredName.trim() !== '') {
+      setEnteredNameIsValid(true);
+      return;
+    }
+  };
+
+  const nameInputBlurHandler = () => {
+    setEnteredNameIsTouched(true);
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+  };
+
+  const formSubmittionHandler = (event) => {
+    event.preventDefault();
+
+    setEnteredNameIsTouched(true);
+
+    if (enteredName.trim() == '') {
+      setEnteredNameIsValid(false);
+      return;
+    }
+    setEnteredName('');
+  };
+
+  const nameInputIsInvalid = enteredNameIsTouched && !enteredNameIsValid;
+
+  const nameInputClasses = nameInputIsInvalid
+    ? 'form-control invalid'
+    : 'form-control';
   return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+    <form onSubmit={formSubmittionHandler}>
+      <div className={nameInputClasses}>
+        <label htmlFor='name'>Your Name: {enteredName}</label>
+        <input
+          type='text'
+          id='name'
+          onChange={nameChangeHandler}
+          onBlur={nameInputBlurHandler}
+          value={enteredName}
+        />
+        {nameInputIsInvalid && (
+          <p className='error-text'>Name should not be empty!</p>
+        )}
       </div>
-      <div className="form-actions">
+      <div className='form-actions'>
         <button>Submit</button>
       </div>
     </form>
